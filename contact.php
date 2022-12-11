a86d7e1e70d3f21e3dea9e907c05dbff8971da52
Batoolaalahmed <106792187+Batoolaalahmed@users.noreply.github.com>
2022-12-11 19:49:06 +0300

    email
    email

--- contact.php
+++ contact.php
@@ -1,26 +1,27 @@
 
 
 
-
 <?php
+
+
+if($_POST["message"]) {
+
+
+mail("batool313@gmail.com", "Here is the subject line",
+
+
+$_POST["insert your message here"]. "From: an@email.address");
+
+
+}
+
+
 $action=$_REQUEST['action'];
 if ($action=="")    /* display the contact form */
-    {
-    ?>
-    <form  action="" method="POST" enctype="multipart/form-data">
-    <input type="hidden" name="action" value="submit">
-    Your name:<br>
-    <input name="name" type="text" value="" size="30"/><br>
-    Your email:<br>
-    <input name="email" type="text" value="" size="30"/><br>
-    Your message:<br>
-    <textarea name="message" rows="7" cols="30"></textarea><br>
-    <input type="submit" value="Send email"/>
-    </form>
-    <?php
-    } 
-else                /* send the submitted data */
-    {
+
+else {            
+   /* send the submitted data */
+  
     $name=$_REQUEST['name'];
     $email=$_REQUEST['email'];
     $message=$_REQUEST['message'];
@@ -38,6 +39,7 @@ else                /* send the submitted data */
 ?>
       
      
+      
 
 
 
